13fa79ad27fe739ea65287baabd13dd5435765e2
Rejoy-CSE-IIT <rejoy.projects@gmail.com>
2017-05-03 19:12:52 +0530

    added constant header file

--- Codes/0001_(14)/a_Blink/a_Blink.ino
+++ Codes/0001_(14)/a_Blink/a_Blink.ino
@@ -1,12 +1,20 @@
-const int ledPin = 13; // LED connected to digital pin 13
+ #include "header/constant.h"
+
+/*
+ * The setup() function is called when a sketch starts. 
+ * Use it to initialize variables, pin modes, 
+ * start using libraries, etc. The setup function will only run once, 
+ * after each powerup or reset of the Arduino board.
+ */
 void setup()
 {
-   pinMode(ledPin, OUTPUT);
+   pinMode(LED_PORT_13, OUTPUT);
 }
+
 void loop()
 {
-    digitalWrite(ledPin, HIGH); // set the LED on
-    delay(2000); // wait for two seconds
-    digitalWrite(ledPin, LOW); // set the LED off
-    delay(3000); // wait for two seconds
+    digitalWrite(LED_PORT_13, HIGH); // set the LED on
+    delay(DELAY_MILLI_SECOND); // wait for two seconds
+    digitalWrite(LED_PORT_13, LOW); // set the LED off
+    delay(DELAY_MILLI_SECOND); // wait for two seconds
 }
